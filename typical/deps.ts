4663db12b882da9e87cc15c38418f64f6cc8324b
Shahid N. Shah <shahid@shah.org>
2020-09-16 18:19:27 -0400

    Update dependencies

--- typical/deps.ts
+++ typical/deps.ts
@@ -1,2 +1,2 @@
-export * as govnImCore from "https://denopkg.com/gov-suite/governed-im@v0.6.1/core/mod.ts";
-export * as govnImTypical from "https://denopkg.com/gov-suite/governed-im@v0.6.1/typical/mod.ts";
+export * as govnImCore from "https://denopkg.com/gov-suite/governed-im@v0.6.2/core/mod.ts";
+export * as govnImTypical from "https://denopkg.com/gov-suite/governed-im@v0.6.2/typical/mod.ts";
