4132afa8ae71cd0886def8d595bdd863203bc6bc
Shahid N. Shah <shahid@shah.org>
2020-09-21 22:17:32 -0400

    Upgrade dependencies

--- models/deps.ts
+++ models/deps.ts
@@ -1,4 +1,4 @@
-export * as govnImCore from "https://denopkg.com/gov-suite/governed-im@v0.6.4/core/mod.ts";
-export * as govnImTypical from "https://denopkg.com/gov-suite/governed-im@v0.6.4/typical/mod.ts";
+export * as govnImCore from "https://denopkg.com/gov-suite/governed-im@v0.6.5/core/mod.ts";
+export * as govnImTypical from "https://denopkg.com/gov-suite/governed-im@v0.6.5/typical/mod.ts";
 export * as contextMgr from "https://denopkg.com/shah/context-manager@v1.0.6/mod.ts";
-export * as namespaceMgr from "https://denopkg.com/shah/namespace-manager@v1.0.6/mod.ts";
+export * as namespaceMgr from "https://denopkg.com/shah/namespace-manager@v1.0.7/mod.ts";
--- rds/deps.ts
+++ rds/deps.ts
@@ -1,8 +1,8 @@
-export * as govnImCore from "https://denopkg.com/gov-suite/governed-im@v0.6.4/core/mod.ts";
-export * as artfPersist from "https://denopkg.com/shah/artifacts-persistence@v1.0.21/mod.ts";
+export * as govnImCore from "https://denopkg.com/gov-suite/governed-im@v0.6.5/core/mod.ts";
+export * as artfPersist from "https://denopkg.com/shah/artifacts-persistence@v1.0.24/mod.ts";
 export * as contextMgr from "https://denopkg.com/shah/context-manager@v1.0.6/mod.ts";
-export * as specModule from "https://denopkg.com/shah/specification-module@v1.0.5/mod.ts";
+export * as specModule from "https://denopkg.com/shah/specification-module@v1.0.6/mod.ts";
 export * as textInflect from "https://denopkg.com/shah/text-inflect@v1.0.4/mod.ts";
-export * as textInterpolate from "https://denopkg.com/shah/text-interpolate@v1.0.6/mod.ts";
-export * as textWhitespace from "https://denopkg.com/shah/text-whitespace@v1.0.2/mod.ts";
-export * as valueMgr from "https://denopkg.com/shah/value-manager@v1.0.7/mod.ts";
+export * as textInterpolate from "https://denopkg.com/shah/text-interpolate@v1.0.7/mod.ts";
+export * as textWhitespace from "https://denopkg.com/shah/text-whitespace@v1.0.3/mod.ts";
+export * as valueMgr from "https://denopkg.com/shah/value-manager@v1.0.8/mod.ts";
