9d3196f9d5e2bff213a6ed1d2fd2d1c1531a2840
divya-sk <divya.kumar@citrusinformatics.com>
2020-11-18 20:43:40 +0530

    Update version changes

--- typical/deps.ts
+++ typical/deps.ts
@@ -1,2 +1,2 @@
-export * as govnImCore from "https://denopkg.com/gov-suite/governed-im@v0.6.5/core/mod.ts";
-export * as govnImTypical from "https://denopkg.com/gov-suite/governed-im@v0.6.5/typical/mod.ts";
+export * as govnImCore from "https://denopkg.com/gov-suite/governed-im@v0.6.6/core/mod.ts";
+export * as govnImTypical from "https://denopkg.com/gov-suite/governed-im@v0.6.6/typical/mod.ts";
