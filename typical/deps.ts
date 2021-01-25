ed7459d6d7800cbf00bd4e666483a1d73c2be515
Shahid N. Shah <shahid@shah.org>
2021-01-25 09:42:59 -0500

    fix: update dependencies

--- typical/deps.ts
+++ typical/deps.ts
@@ -1,3 +1,3 @@
-export * as govnImCore from "https://denopkg.com/gov-suite/governed-im@v0.7.0/core/mod.ts";
-export * as govnImTypical from "https://denopkg.com/gov-suite/governed-im@v0.7.0/typical/mod.ts";
+export * as govnImCore from "https://denopkg.com/gov-suite/governed-im@v0.7.1/core/mod.ts";
+export * as govnImTypical from "https://denopkg.com/gov-suite/governed-im@v0.7.1/typical/mod.ts";
 export * as safety from "https://denopkg.com/shah/ts-safety@v0.3.1/mod.ts";
