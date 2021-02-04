6e796287ae67f035f282088a60be73edf2db01e7
Shahid N. Shah <shahid@shah.org>
2021-02-03 19:23:25 -0500

    fix: don't use async without await

--- models/mod_test.ts
+++ models/mod_test.ts
@@ -1,7 +1,7 @@
 import * as tm from "./test-model.gim.ts";
 import { testingAsserts as ta } from "./deps-test.ts";
 
-Deno.test("TestModelStructure validation", async () => {
+Deno.test("TestModelStructure validation", () => {
   const model = new tm.TestModel();
   // TODO: validate contents of the model, not that it just parsed
   ta.assert(model);
