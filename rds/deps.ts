383bcd2f3a6b22d5fdaf21de7e4006f2b0a87b1b
SumiSujith <sumi.mathew@citrusinformatics.com>
2021-04-07 11:36:44 +0530

    Update latest dependencies

--- rds/deps.ts
+++ rds/deps.ts
@@ -1,4 +1,4 @@
-export * as govnImCore from "https://denopkg.com/gov-suite/governed-im@v0.7.2/core/mod.ts";
+export * as govnImCore from "https://denopkg.com/gov-suite/governed-im@v0.7.3/core/mod.ts";
 export * as artfPersist from "https://denopkg.com/shah/artifacts-persistence@v1.2.0/mod.ts";
 export * as contextMgr from "https://denopkg.com/shah/context-manager@v1.0.7/mod.ts";
 export * as specModule from "https://denopkg.com/shah/specification-module@v1.0.9/mod.ts";
